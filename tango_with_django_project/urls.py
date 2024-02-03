5e78c97b41b28dc4995840c0ea289bc09ea2eac6
“Zhuofan <2512331m@student.gla.ac.ukgit config --global user.email 2512331m@student.gla.ac.uk>
2024-02-03 13:43:59 +0000

    5

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 from rango import views
+from django.conf import settings
+from django.conf.urls.static import static
 """tango_with_django_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -26,4 +28,4 @@ urlpatterns = [
     path('rango/', include('rango.urls')),
     # The above maps any URLs starting with rango/ to be handled by rango.
     path('admin/', admin.site.urls),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
